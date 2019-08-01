8a1ff76a6c1107747a614d7a05899ee3606eaac8
leodario <leodario3@hotmail.com>
2019-08-01 15:15:08 -0300

    consulta CEP
    atualizando consulta cep

--- consulta_cep/quickstart/web/main.dart
+++ consulta_cep/quickstart/web/main.dart
@@ -1,5 +1,22 @@
 import 'dart:html';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  
+  querySelector('#search').onClick.listen((a) async{
+
+    String cep = (querySelector('#cep') as InputElement).value;
+    String url = 'http://viacep.com.br/ws/$cep/json/';
+
+    var response = await http.get(url);
+    var body = json.decode(response.body);
+    print(body);
+
+    (querySelector('#state') as InputElement).value = body['uf'];
+    (querySelector('#city') as InputElement).value = body['localidade'];
+    (querySelector('#neighborhood') as InputElement).value = body['bairro'];
+    (querySelector('#street') as InputElement).value = body['logradouro'];
+    
+  });
 }
